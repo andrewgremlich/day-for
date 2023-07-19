1ba344d240e308c2a641c10f84387a724c55ed5f
andrewgremlich <andrew.gremlich@gmail.com>
2023-07-19 09:17:46 -0600

    snow endpoint implemented

--- src/day-for/snow.ts
+++ src/day-for/snow.ts
@@ -2,7 +2,14 @@ import { Router, load } from "../../deps.ts";
 
 const env = await load();
 const PIRATE_WEATER_API_KEY = env.PIRATE_WEATER_API_KEY;
+const PIRATE_WEATHER_API_URL = env.PIRATE_WEATHER_API_URL;
 
-export const dayForSnowRouter = new Router().get("/:timestamp", (ctx) => {
-  ctx.response.body = { isSnowDay: false, API_KEY: PIRATE_WEATER_API_KEY };
+export const dayForSnowRouter = new Router().get("/", async (ctx) => {
+  const lat = ctx.request.url.searchParams.get('lat')
+  const long = ctx.request.url.searchParams.get('long')
+
+  const fetchWeather = await fetch(`${PIRATE_WEATHER_API_URL}/${PIRATE_WEATER_API_KEY}/${lat},${long}`)
+  const weather = await fetchWeather.json();
+
+  ctx.response.body = { isSnowDay: weather.minutely.icon === 'snow', icon: weather.minutely.icon };
 })
