65aceafee824f17c882416dc59ab0ef175acbe51
andrewgremlich <andrew.gremlich@gmail.com>
2023-07-20 14:43:28 -0600

    caching for snow day

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { load } from "https://deno.land/std/dotenv/mod.ts";
+import Redis from "npm:ioredis";
 
-export { Application, Router, load };
+export { Application, Router, load, Redis };
--- src/day-for/snow.ts
+++ src/day-for/snow.ts
@@ -1,15 +1,42 @@
-import { Router, load } from "../../deps.ts";
+import { Router, load, Redis } from "../../deps.ts";
 
 const env = await load();
 const PIRATE_WEATER_API_KEY = env.PIRATE_WEATER_API_KEY;
 const PIRATE_WEATHER_API_URL = env.PIRATE_WEATHER_API_URL;
+const UPSTASH_USER = env.UPSTASH_USER;
+const UPSTASH_PASSWORD = env.UPSTASH_PASSWORD;
+const UPSTASH_ENDPOINT = env.UPSTASH_ENDPOINT;
+const UPSTASH_PORT = env.UPSTASH_PORT;
 
 export const dayForSnowRouter = new Router().get("/", async (ctx) => {
-  const lat = ctx.request.url.searchParams.get('lat')
-  const long = ctx.request.url.searchParams.get('long')
+  const client = new Redis(`rediss://${UPSTASH_USER}:${UPSTASH_PASSWORD}@${UPSTASH_ENDPOINT}:${UPSTASH_PORT}`);
+  const lat = ctx.request.url.searchParams.get('lat');
+  const long = ctx.request.url.searchParams.get('long');
+  const redisKey = `isSnowDay:${lat}:${long}`;
+  const isCached = await client.exists(redisKey);
 
-  const fetchWeather = await fetch(`${PIRATE_WEATHER_API_URL}/${PIRATE_WEATER_API_KEY}/${lat},${long}`)
-  const weather = await fetchWeather.json();
+  if (isCached) {
+    const cachedValue = await client.get(redisKey);
+    ctx.response.body = JSON.parse(cachedValue);
+    return;
+  }
 
-  ctx.response.body = { isSnowDay: weather.minutely.icon === 'snow', icon: weather.minutely.icon };
+  try {
+    const fetchWeather = await fetch(`${PIRATE_WEATHER_API_URL}/${PIRATE_WEATER_API_KEY}/${lat},${long}?units=si&exclude=hourly,minutely,daily,alerts`);
+    const weather = await fetchWeather.json();
+
+    if (fetchWeather.ok === false) {
+      ctx.response.status = 500;
+      ctx.response.body = { isSnowDay: false, error: weather.error };
+      return;
+    }
+
+    const returnBody = { isSnowDay: weather.currently.icon === 'snow', icon: weather.currently.icon };
+
+    await client.set(redisKey, JSON.stringify(returnBody));
+
+    ctx.response.body = returnBody;
+  } catch (err) {
+    ctx.response.body = { isSnowDay: false, error: err.toString() };
+  }
 })
